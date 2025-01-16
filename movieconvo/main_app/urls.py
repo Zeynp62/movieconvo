edbd2024aba2aab3e937eb8365f35966a11eb4aa
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2025-01-16 08:00:42 +0300

    profile

--- movieconvo/main_app/urls.py
+++ movieconvo/main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import profile
 
 urlpatterns=[
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
-    path('accounts/signup/',views.signup,name='signup')
+    path('accounts/signup/',views.signup,name='signup'),
+    path('profile/', profile, name='profile'),
     
 ]
--- movieconvo/main_app/views.py
+++ movieconvo/main_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
 
 def home(request):
     return render(request,'home.html')
